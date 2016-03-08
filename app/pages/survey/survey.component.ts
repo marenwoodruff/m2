1200ea0c792fd7653f7506a88e2646967c6044e7
Cori Fowler <fowler.cori@gmail.com>
2016-03-08 17:54:57 -0500

    added outline of survey component and question directive

--- app/pages/survey/survey.component.ts
+++ app/pages/survey/survey.component.ts
@@ -1,7 +1,11 @@
 import {Page} from 'ionic-angular';
 
+import {QuestionDirective} from '../../directives/question.directive';
+
 
 @Page({
-  templateUrl: 'build/pages/survey/survey.component.html'
+  templateUrl: 'build/pages/survey/survey.component.html',
+  directives: [QuestionDirective],
 })
+
 export class SurveyComponent { }
