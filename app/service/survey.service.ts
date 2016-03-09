9ee1685644546370defea415afbb084ad5c0e549
Abbey Gwayambadde <ophedian@gmail.com>
2016-03-09 12:33:16 -0500

    Adding method stubs to the Survey Service

--- app/service/survey.service.ts
+++ app/service/survey.service.ts
@@ -6,8 +6,36 @@ export module Matrix.MyMatrix {
 
     @Injectable()
     export class SurveyService {
-        constructor(private http:Http) {
 
-        }
+        constructor(private http:Http) {
+        };
+
+        getSurveys = (id?:number):any[] => {
+            //TODO: change return type to array of Survey
+
+            return null;
+        };
+
+        getSurveyResponses = (id:number, userId?:number):any => {
+            //TODO: change return type to array of Question[]
+
+            return null;
+        };
+
+        saveSurveyProgress = (id:any):boolean => {
+            return null;
+        };
+
+        getSurveyProgress = (id:any):any =>{
+            //TODO: change return type to SurveyProgress
+
+            return null;
+
+        };
+
+        saveSurvey = (survey:any):boolean => {
+            //TODO: change survey param type to Survey type
+            return null;
+        };
     }
 }
