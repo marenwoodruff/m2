1a9925d1bef25b26349076ceeaf0fe3054c30ce2
Cori Fowler <fowler.cori@gmail.com>
2016-03-09 17:34:49 -0500

    removed namespace from services

--- app/service/credentials.service.ts
+++ app/service/credentials.service.ts
@@ -1,11 +1,10 @@
 import {Injectable} from 'angular2/core';
 
-export module Matrix.MyMatrix {
+@Injectable()
+export class CredentialService {
+    constructor() {
 
-    @Injectable()
-    export class CredentialService {
-        constructor() {
-
-        }
     }
 }
+
+
--- app/service/user.service.ts
+++ app/service/user.service.ts
@@ -1,11 +1,9 @@
 import {Injectable} from 'angular2/core';
 
-export module Matrix.MyMatrix {
+@Injectable()
+export class UserService {
+    constructor() {
 
-    @Injectable()
-    export class UserService {
-        constructor() {
-
-        }
     }
 }
+
