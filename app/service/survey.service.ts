5d8f04d957533bda53f4059377e5e696c64cfccf
Cori Fowler <fowler.cori@gmail.com>
2016-03-09 17:32:54 -0500

    added survey service to app.ts file, removed module syntax from components

--- app/service/survey.service.ts
+++ app/service/survey.service.ts
@@ -2,40 +2,40 @@ import {Injectable} from 'angular2/core';
 import {Http} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
 
-export module Matrix.MyMatrix {
+@Injectable()
+export class SurveyService {
 
-    @Injectable()
-    export class SurveyService {
+    constructor(private http:Http) {
+    };
 
-        constructor(private http:Http) {
-        };
+    getSurveys = (id?:number):any[] => {
+        //TODO: change return type to array of Survey
 
-        getSurveys = (id?:number):any[] => {
-            //TODO: change return type to array of Survey
+        return null;
+    };
 
-            return null;
-        };
+    getSurveyResponses = (id:number, userId?:number):any => {
+        //TODO: change return type to array of Question[]
 
-        getSurveyResponses = (id:number, userId?:number):any => {
-            //TODO: change return type to array of Question[]
+        return null;
+    };
 
-            return null;
-        };
+    saveSurveyProgress = (id:any):boolean => {
+        return null;
+    };
 
-        saveSurveyProgress = (id:any):boolean => {
-            return null;
-        };
+    getSurveyProgress = (id:any):any =>{
+        //TODO: change return type to SurveyProgress
 
-        getSurveyProgress = (id:any):any =>{
-            //TODO: change return type to SurveyProgress
+        return null;
 
-            return null;
+    };
 
-        };
-
-        saveSurvey = (survey:any):boolean => {
-            //TODO: change survey param type to Survey type
-            return null;
-        };
-    }
+    saveSurvey = (survey:any):boolean => {
+        //TODO: change survey param type to Survey type
+        return null;
+    };
 }
+
+
+
