6369e5cb33e4038e28902fffe1a8671b4a69e200
Justin <justinowenjacot@icloud.com>
2016-03-10 14:09:38 -0500

    reconciled the files

--- app/modules/survey/surveyResponse.ts
+++ app/modules/survey/surveyResponse.ts
@@ -1,20 +1,15 @@
-//<reference path="./response.ts" />
-//<reference path="./user.ts" />
-
-import {User} from "./../user";
+import {User} from "./user";
 import {Response} from "./response";
 
-module Matrix.MyMatrix {
+export class SurveyResponse {
+    user:User;
+    surveyId:number;
+    responses:Response[];
 
-    export class SurveyResponse {
-        user:User;
-        surveyId:number;
-        responses:Response[];
-
-        constructor(user?:User, surveyId?:number, responses?:Response[]) {
-            this.user = user;
-            this.surveyId = surveyId;
-            this.responses = responses;
-        }
+    constructor(user?:User, surveyId?:number, responses?:Response[]) {
+        this.user = user;
+        this.surveyId = surveyId;
+        this.responses = responses;
     }
 }
+
