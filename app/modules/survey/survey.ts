9772f17328c8006436cbfe60e55bd63c5f71af42
Justin <justinowenjacot@icloud.com>
2016-03-10 14:09:38 -0500

    reconciled the files

--- app/modules/survey/survey.ts
+++ app/modules/survey/survey.ts
@@ -1,7 +1,13 @@
 import {Question} from "./question";
 
 export class Survey {
-    Id:number;
+    id:number;
     name:string;
     questions:Question[];
+
+    constructor(id?: number, name?: string, questions?: Question[]) {
+      this.id = id;
+      this.name = name;
+      this.questions = questions;
+    }
 }
