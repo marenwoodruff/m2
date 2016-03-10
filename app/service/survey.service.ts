416259ca3f506eaff9b8c0a1de348d0f1c674580
Isaac Hardy <isaachardyDEV@gmail.com>
2016-03-10 16:58:44 -0500

    imported http providers from ng2

--- app/service/survey.service.ts
+++ app/service/survey.service.ts
@@ -1,5 +1,5 @@
 import {Injectable, EventEmitter} from 'angular2/core';
-import {Http} from 'angular2/http';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
