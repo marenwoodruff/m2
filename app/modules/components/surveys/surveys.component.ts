b665a6e56ca1f0689a7b83b3243609f07fdfb4a1
Cori Fowler <fowler.cori@gmail.com>
2016-03-11 14:38:05 -0500

    updated survey.json to remove trailing commas and other syntax

--- app/modules/components/surveys/surveys.component.ts
+++ app/modules/components/surveys/surveys.component.ts
@@ -1,6 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit, OnDestroy} from 'angular2/core';
 import {SurveyComponent} from '../survey/survey.component';
 import {SurveyService} from '../../../service/survey.service';
+import {Survey} from '../../survey/survey';
 
 @Component({
   selector: 'surveys',
@@ -9,12 +10,27 @@ import {SurveyService} from '../../../service/survey.service';
   providers: [SurveyService]
 })
 
-export class SurveysComponent { 
+export class SurveysComponent implements OnInit, OnDestroy { 
 
   private _surveyApi: SurveyService;
+  public surveys: Survey[];
 
   constructor(surveyService:SurveyService) {
     this._surveyApi = surveyService;
+  }
+
+  ngOnInit():any {
+    this._surveyApi.surveys.subscribe(
+      surveys => this.surveys = surveys,
+      err => console.log('EventsComponent events subscribe error:', err),
+      () => console.log('finished subscribing to events')
+    );
+
+    this._surveyApi.getSurveys();
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
