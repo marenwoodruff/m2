596cd9ba7a32d96a365c8648010d56e203dcf0f2
Cori Fowler <fowler.cori@gmail.com>
2016-03-14 14:32:48 -0400

    survey displays in its own view - using navcontroller and params

--- app/modules/components/survey/survey.component.ts
+++ app/modules/components/survey/survey.component.ts
@@ -1,16 +1,30 @@
 import {Component} from 'angular2/core';
-import {Item} from 'ionic-angular';
+import {Item, NavController} from 'ionic-angular';
 import {Survey} from '../../survey/survey';
 import {QuestionComponent} from '../question/question.component';
 
 @Component({
   selector: 'survey',
   templateUrl: 'build/modules/components/survey/survey.component.html',
-  directives: [Item, QuestionComponent],
+  directives: [Item],
   inputs:['survey']
 })
 
 export class SurveyComponent { 
   survey: Survey;
+  nav: any;
+
+  constructor(nav: NavController) { 
+    this.nav = nav;
+    console.log(this.nav);
+  }
+
+  viewSurvey(survey) {
+    console.log(survey);
+    this.nav.push(QuestionComponent, {
+      question: survey
+    });
+  }
+
 }
 
