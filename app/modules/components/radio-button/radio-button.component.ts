e8cf9d77faa56578b214927f0992cdb0d65935c1
Cori Fowler <fowler.cori@gmail.com>
2016-03-15 15:17:22 -0400

    updated radio button component so only one choice can be selected at a time

--- app/modules/components/radio-button/radio-button.component.ts
+++ app/modules/components/radio-button/radio-button.component.ts
@@ -1,12 +1,13 @@
 import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES, FORM_PROVIDERS} from 'angular2/common';
 import {Item, Label, RadioButton} from 'ionic-angular';
 import {Answer} from '../../survey/answer';
 
 @Component({
   selector: 'radio-button',
   templateUrl: 'build/modules/components/radio-button/radio-button.component.html',
-  directives: [Item, Label, RadioButton],
-  inputs: ['answer']
+  directives: [FORM_DIRECTIVES, Item, Label, RadioButton],
+  inputs: ['answer'],
 })
 
 export class RadioButtonComponent {
