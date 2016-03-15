e11dd078eebb3b2c61a70a3cd3742f7c6f0e77aa
Cori Fowler <fowler.cori@gmail.com>
2016-03-15 15:17:22 -0400

    updated radio button component so only one choice can be selected at a time

--- app/modules/components/answer/answer.component.ts
+++ app/modules/components/answer/answer.component.ts
@@ -1,6 +1,6 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Answer} from '../../survey/answer';
-import {Item, Label, RadioGroup} from 'ionic-angular';
+import {Item, Label, RadioGroup, RadioButton} from 'ionic-angular';
 import {RadioButtonComponent} from '../radio-button/radio-button.component';
 import {TextboxComponent} from '../textbox/textbox.component';
 import {CheckboxComponent} from '../checkbox/checkbox.component';
@@ -8,12 +8,17 @@ import {CheckboxComponent} from '../checkbox/checkbox.component';
 @Component({
   selector: 'answer',
   templateUrl: 'build/modules/components/answer/answer.component.html',
-  directives: [Item, Label, RadioGroup, RadioButtonComponent, TextboxComponent, CheckboxComponent],
+  directives: [Item, Label, RadioGroup, RadioButton, RadioButtonComponent, TextboxComponent, CheckboxComponent],
   inputs: ['answer']
 })
 
-export class AnswerComponent { 
+export class AnswerComponent implements OnInit { 
   answer: Answer;
+
+  ngOnInit() {
+    console.log(this.answer);
+  }
+
 
 }
 
