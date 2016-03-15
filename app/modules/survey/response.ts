53cefa6a56573a8cf9f9e0bcd8a0d0f3d66cd129
Isaac Hardy <isaachardyDEV@gmail.com>
2016-03-15 13:27:00 -0400

    Added function to save survey to local storage and to make a http request to submit a survey when finished

--- app/modules/survey/response.ts
+++ app/modules/survey/response.ts
@@ -2,7 +2,6 @@ export class Response {
     questionId:number;
     selections:Selection[];
 
-
     constructor(questionId?:number, selections?:Selection[]) {
         this.questionId = questionId;
         this.selections = selections;
