5b9bb0c07687e6fe372796b14e7a93fdcee25a6a
Cori Fowler <fowler.cori@gmail.com>
2016-03-15 13:25:41 -0400

    textbox component created

--- app/modules/components/answer/answer.component.ts
+++ app/modules/components/answer/answer.component.ts
@@ -2,11 +2,12 @@ import {Component} from 'angular2/core';
 import {Answer} from '../../survey/answer';
 import {Item, Label, RadioGroup} from 'ionic-angular';
 import {RadioButtonComponent} from '../radio-button/radio-button.component';
+import {TextboxComponent} from '../textbox/textbox.component';
 
 @Component({
   selector: 'answer',
   templateUrl: 'build/modules/components/answer/answer.component.html',
-  directives: [Item, Label, RadioGroup, RadioButtonComponent],
+  directives: [Item, Label, RadioGroup, RadioButtonComponent, TextboxComponent],
   inputs: ['answer']
 })
 
