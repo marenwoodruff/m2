cfeacd8d817273eda887063d3f8de3cb9bdff975
Cori Fowler <fowler.cori@gmail.com>
2016-03-15 16:04:47 -0400

    updated file structure to be more comprehensive, push animation working

--- app/modules/components/question/question.component.ts
+++ app/modules/components/question/question.component.ts
@@ -3,28 +3,16 @@ import {Page, NavParams} from 'ionic-angular';
 import {Question} from '../../survey/question';
 import {AnswerComponent} from '../answer/answer.component';
 
-// @Component({
-//   selector: 'question',
-//   templateUrl: 'build/modules/components/question/question.component.html',
-//   directives: [AnswerComponent],
-// })
-
-@Page({
+@Component({
+  selector: 'question',
   templateUrl: 'build/modules/components/question/question.component.html',
-  directives: [AnswerComponent]
+  directives: [AnswerComponent],
+  inputs: ['question']
 })
 
 
 export class QuestionComponent { 
   question: Question;
-  params: NavParams;
-  questions: Question;
-
-  constructor(params: NavParams) {
-    this.params = params;
-    this.questions = this.params.get('question');
-    console.log(this.questions);
-  }
 }
   
 
