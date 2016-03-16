6602cb9ad2d85566162d618fa64833d0c2d5cfbe
Isaac Hardy <isaachardyDEV@gmail.com>
2016-03-16 10:07:37 -0400

    Implemented twitter service and component

--- app/pages/twitter/twitter.page.ts
+++ app/pages/twitter/twitter.page.ts
@@ -1,21 +1,9 @@
 import {Page} from 'ionic-angular';
-import {OnInit} from 'angular2/core';
+import {TwitterComponent} from '../../modules/components/twitter/twitter.component';
 
 @Page({
-  templateUrl: 'build/pages/twitter/twitter.page.html'
+  templateUrl: 'build/pages/twitter/twitter.page.html',
+  directives: [TwitterComponent]
 })
 
-export class TwitterPage {
-  constructor() {
-    !function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0],
-      p = /^http:/.test(d.location) ? 'http' : 'https';
-      if (!d.getElementById(id)) {
-        js = d.createElement(s);
-        js.id = id;
-        js.src = p + "://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
-      }
-    }(document, "script", "twitter-wjs");
-  }
-}
+export class TwitterPage { }
