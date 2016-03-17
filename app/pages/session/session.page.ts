8b3cfe077523e8030054355a63033c52b2ba3e5c
Isaac Hardy <isaachardyDEV@gmail.com>
2016-03-17 13:33:50 -0400

    Fixed issues with file relocation

--- app/pages/session/session.page.ts
+++ app/pages/session/session.page.ts
@@ -1,10 +1,8 @@
 import {Page} from 'ionic-angular';
 import {Session} from '../../models/events/session';
-import {SessionComponent} from '../../components/session/session.component';
 import {NavParams} from 'ionic-angular';
 @Page({
     templateUrl: 'build/pages/session/session.page.html',
-    directives: [SessionComponent]
 })
 export class SessionPage {
   params: NavParams;
