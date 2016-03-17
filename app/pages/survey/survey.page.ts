f1050c34d5217ffc27dddb1c980a10c3e600b1c6
Cori Fowler <fowler.cori@gmail.com>
2016-03-17 13:56:21 -0400

    working on saving responses to original survey

--- app/pages/survey/survey.page.ts
+++ app/pages/survey/survey.page.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core';
 import {Page, NavParams} from 'ionic-angular';
 import {Question} from '../../modules/survey/question';
+import {Survey} from '../../modules/survey/survey';
 import {QuestionComponent} from '../../modules/components/question/question.component';
 
 @Page({
@@ -10,12 +11,12 @@ import {QuestionComponent} from '../../modules/components/question/question.comp
 
 
 export class SurveyPage{
-  question: Question;
   params: NavParams;
-  questions: Question;
+  survey: Survey;
 
   constructor(params: NavParams) {
     this.params = params;
-    this.questions = this.params.get('question');
+    this.survey = this.params.get('question');
+    console.log('survey:', this.survey);
   }
 }
