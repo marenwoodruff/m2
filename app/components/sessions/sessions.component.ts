1db6950a0a7c81418ebc31912ec99295ae0c7160
Nick Leach <nickleach22@gmail.com>
2016-03-18 11:52:37 -0400

    added button to sessions list

--- app/components/sessions/sessions.component.ts
+++ app/components/sessions/sessions.component.ts
@@ -1,14 +1,21 @@
 import {Component} from 'angular2/core';
-import {List, Item} from 'ionic-angular';
+import {List, Item, Button, Icon, NavController} from 'ionic-angular';
 import {DateFormatPipe} from 'angular2-moment';
 
 @Component({
   selector: 'sessions',
   templateUrl: 'build/components/sessions/sessions.component.html',
-  directives: [List, Item],
+  directives: [List, Item, Button, Icon],
   inputs: ['sessions'],
   pipes: [DateFormatPipe]
 })
 
 
-export class SessionsComponent {}
+export class SessionsComponent {
+  navController: NavController;
+
+  constructor(navController: NavController){
+    this.navController = navController;
+  }
+
+}
