595ee16cef33f06ff86391049e741280b2498fc4
Cori Fowler <fowler.cori@gmail.com>
2016-03-18 16:20:32 -0400

    updated single letter variables and question input

--- app/components/textbox/textbox.component.ts
+++ app/components/textbox/textbox.component.ts
@@ -1,24 +1,19 @@
 import {Component} from 'angular2/core';
 import {Item, Label, TextArea} from 'ionic-angular';
-import {Answer} from '../../models/survey/answer';
-import {Response} from '../../models/survey/response';
-import {Selection} from '../../models/survey/selection';
+import {Question} from '../../models/survey/question';
 
 @Component({
   selector: 'textbox',
   templateUrl: 'build/components/textbox/textbox.component.html',
   directives: [Item, Label, TextArea],
-  inputs: ['answer']
+  inputs: ['question']
 })
 
 export class TextboxComponent {
-  answer: Answer;
-  response: Response;
-  selection: Selection;
+  question: Question;
+
 
   getValue(id: any, val: string) {
-    let selection = new Selection(val);
-    let response = new Response(id, [selection]);
     id.value = val;
     console.log(id, val);
   }
