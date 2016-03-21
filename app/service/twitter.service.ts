d7336d252b2eeff318285be79a1837409ed45712
Isaac Hardy <isaachardyDEV@gmail.com>
2016-03-21 12:44:54 -0400

    Added sql to storage service

--- app/service/twitter.service.ts
+++ app/service/twitter.service.ts
@@ -16,32 +16,35 @@ export class TwitterService {
       this._api = http;
     }
     getBearer() {
-      let key = "Z3Kq0g7X3Py6H1Ot089DZQ4vh";
-      let secret = "6vMzyJkVk7Z7SgAC0r6jcl8r6YUqafHbYtIJ16bRkowHAI8pNz";
-
-      let bearerRequest = btoa(key + ':' + secret);
-      console.log(bearerRequest)
-
-      let headers = new Headers();
-      headers.append("Authorization", "Basic " + bearerRequest);
-      headers.append("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-      console.log('headers', headers);
-
-      this._api.post('https://api.twitter.com/oauth2/token', "grant_type=client_credentials", {headers: headers})
-        .map(res => res.json())
-        .subscribe(
-          bearer => this.bearerToken.emit(bearer),
-          err => console.log(err),
-          () => console.log('Bearer Token retrieval is completed'));
+      // let key = "Z3Kq0g7X3Py6H1Ot089DZQ4vh";
+      // let secret = "6vMzyJkVk7Z7SgAC0r6jcl8r6YUqafHbYtIJ16bRkowHAI8pNz";
+      //
+      // let bearerRequest = btoa(key + ':' + secret);
+      // console.log(bearerRequest)
+      //
+      // let headers = new Headers();
+      // headers.append("Authorization", "Basic " + bearerRequest);
+      // headers.append("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+      // console.log('headers', headers);
+      //
+      // this._api.post('https://api.twitter.com/oauth2/token', "grant_type=client_credentials", {headers: headers})
+      //   .map(res => console.log('bearerToken', res))
+      //   .subscribe(
+      //     bearer => console.log('bearerToken', bearer),
+      //     err => console.log('this is the error', err),
+      //     () => console.log('Bearer Token retrieval is completed'));
     }
     getMatrixFeed() {
-      let testing = this.getBearer();
-      console.log("testing", testing);
-      // this._api.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=matrixresources&count=2')
-      //   .map(res => <TwitterFeed[]> res.json())
-      //   .subscribe(
-      //       matrixFeed =>this.feed.emit(matrixFeed),
-      //       err => console.log(err),
-      //       () => console.log('Matrix Twitter Feed retrieval is completed'));
+
+      let headers = new Headers();
+      headers.append("Authorization", "Bearer AAAAAAAAAAAAAAAAAAAAABWruAAAAAAAmYTQM0oq16eaD%2FKtouB6FzJmVRI%3Dc6vo5GlwjWZWEc27ALYnoBI2M1ETx0UvvSMo8T50iHTPiPcE4F");
+      headers.append("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+
+      this._api.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=matrixresources', headers)
+        .map(res => <TwitterFeed[]> res.json())
+        .subscribe(
+            matrixFeed =>this.feed.emit(matrixFeed),
+            err => console.log(err),
+            () => console.log('Matrix Twitter Feed retrieval is completed'));
     }
 }
