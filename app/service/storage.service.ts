7e32901f589f27ec47a14b9e6e4218119931fb9b
Isaac Hardy <isaachardyDEV@gmail.com>
2016-03-21 13:41:59 -0400

    Fixed issues in storage service

--- app/service/storage.service.ts
+++ app/service/storage.service.ts
@@ -14,17 +14,16 @@ export class StorageService {
   private storage: Storage;
 
     constructor() {
-      this.storage = new Storage(SqlStorage, {name: 'responses'});
-      this.storage.query('CREATE TABLE IF NOT EXISTS responses (id INTEGER PRIMARY KEY AUTOINCREMENT, surveyId TEXT, responses TEXT)')
+      this.storage = new Storage(SqlStorage, {name: 'SurveyResponse'});
+      this.storage.query('CREATE TABLE IF NOT EXISTS SurveyResponse (id INTEGER PRIMARY KEY AUTOINCREMENT, surveyId TEXT, responses TEXT)')
         .then((data) => {
           console.log("TABLE CREATED -> " + JSON.stringify(data.res));
         }, (error) => {
           console.log("ERROR -> " + JSON.stringify(error.err));
         });
     }
-    public saveSurveyProgress(res: Survey):void {
-      let data = res;
-      this.storage.query(`INSERT INTO responses (surveyId, responses) VALUES (${data.id}, ${data.questions})`)
+    public saveSurveyProgress(survey: Survey):void {
+      this.storage.query(`INSERT INTO SurveyResponse (surveyId, responses) VALUES (${survey.id}, ${survey.questions})`)
         .then((data) => {
           this.surveyQuestions.emit(data);
           console.log(JSON.stringify(data.res));
@@ -33,7 +32,7 @@ export class StorageService {
         });
     }
     public getSurveyProgress(id:any):void {
-      this.storage.query('SELECT * FROM responses')
+      this.storage.query('SELECT * FROM SurveyResponse')
         .then((data) => {
           this.surveyProgress.emit(data);
           console.log("Data: " + data);
