59209728cb768b72a954ec8f8c4af05475941556
Cori Fowler <fowler.cori@gmail.com>
2016-03-22 15:37:43 -0400

    switched question component for survey component on survey page

--- app/pages/survey/survey.page.ts
+++ app/pages/survey/survey.page.ts
@@ -2,11 +2,12 @@ import {Component} from 'angular2/core';
 import {Page, NavParams} from 'ionic-angular';
 import {Question} from '../../models/survey/question';
 import {QuestionComponent} from '../../components/question/question.component';
+import {SurveyComponent} from '../../components/survey/survey.component';
 import {Survey} from '../../models/survey/survey';
 
 @Page({
   templateUrl: 'build/pages/survey/survey.page.html',
-  directives: [QuestionComponent]
+  directives: [QuestionComponent, SurveyComponent]
 })
 
 
@@ -16,6 +17,6 @@ export class SurveyPage{
 
   constructor(params: NavParams) {
     this.params = params;
-    this.survey = this.params.get('question');
+    this.survey = this.params.get('survey');
   }
 }
