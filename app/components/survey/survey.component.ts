1f0a7a2a9e85ab6da286e895b02cc1b2f9e50476
Cori Fowler <fowler.cori@gmail.com>
2016-03-22 11:58:39 -0400

    added survey component folder/files to begin survey refactor

---
+++ app/components/survey/survey.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'survey',
+  templateUrl: 'build/components/survey/survey.component.html'
+})
+
+export class SurveyComponent {}
