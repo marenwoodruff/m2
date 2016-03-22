96843c648d77899f3bb946c0a1e08d6fadfb8229
Cori Fowler <fowler.cori@gmail.com>
2016-03-22 15:55:44 -0400

    added radio buttons to refactored survey

--- app/components/survey/survey.component.ts
+++ app/components/survey/survey.component.ts
@@ -1,14 +1,16 @@
 import {Component} from 'angular2/core';
-import {Button} from 'ionic-angular';
+import {Button, Item, Label} from 'ionic-angular';
+import {Survey} from '../../models/survey/survey';
 
 @Component({
   selector: 'survey',
   templateUrl: 'build/components/survey/survey.component.html',
-  directives: [Button],
+  directives: [Button, Item, Label],
   inputs: ['survey']
 })
 
 export class SurveyComponent {
+  survey: Survey
 
   saveProgress(survey) {
     console.log(survey);
