3c8c4eb75a5921bfa091bec2087b8031eb951138
Cori Fowler <fowler.cori@gmail.com>
2016-03-23 14:09:10 -0400

    forms validating with issue with ng controls for multiple questions of same type

--- app/components/survey/survey.component.ts
+++ app/components/survey/survey.component.ts
@@ -1,19 +1,35 @@
 import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES, FormBuilder, Validators, AbstractControl, ControlGroup} from 'angular2/common';
 import {Button, Item, Label, TextArea, Checkbox} from 'ionic-angular';
 import {Survey} from '../../models/survey/survey';
 
 @Component({
   selector: 'survey',
   templateUrl: 'build/components/survey/survey.component.html',
-  directives: [Button, Item, Label, TextArea, Checkbox],
+  directives: [FORM_DIRECTIVES, Button, Item, Label, TextArea, Checkbox],
   inputs: ['survey']
 })
 
 export class SurveyComponent {
   survey: Survey
+  surveyForm: ControlGroup;
+  radio: AbstractControl;
+  textBox: AbstractControl;
+  checkBox: AbstractControl;
+
+  constructor(builder: FormBuilder) {
+    this.surveyForm = builder.group({
+      'radio': ['', Validators.required],
+      'textBox': ['', Validators.minLength(5)],
+      'checkBox': ['', Validators.required]
+    });
+
+    this.radio = this.surveyForm.controls['radio'];
+    this.textBox = this.surveyForm.controls['textBox'];
+    this.checkBox = this.surveyForm.controls['checkBox'];
+  }
 
   selectOption(options, option) {
-
     options.forEach(function(opt) {
       if (opt.value === option.value) {
         opt.selected = true;
@@ -22,10 +38,23 @@ export class SurveyComponent {
       }
       return opt;
     });
+  }
 
+  checkOption(option) {
+    if (option.selected) {
+      option.selected = false;
+    } else {
+      option.selected = true;
+    }
+    console.log(option);
+    return option.selected;
   }
 
   saveProgress(survey) {
     console.log(survey);
+  }
+
+  onSubmit(survey) {
+    console.log('submitting: ', survey);
   }
 }
