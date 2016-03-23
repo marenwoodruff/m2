8dbc5d8e80b0a43fbdb98ffaaa4ec8a169bea664
Cori Fowler <fowler.cori@gmail.com>
2016-03-23 11:02:41 -0400

    added all answer types to survey component

--- app/components/survey/survey.component.ts
+++ app/components/survey/survey.component.ts
@@ -1,11 +1,11 @@
 import {Component} from 'angular2/core';
-import {Button, Item, Label} from 'ionic-angular';
+import {Button, Item, Label, TextArea, Checkbox} from 'ionic-angular';
 import {Survey} from '../../models/survey/survey';
 
 @Component({
   selector: 'survey',
   templateUrl: 'build/components/survey/survey.component.html',
-  directives: [Button, Item, Label],
+  directives: [Button, Item, Label, TextArea, Checkbox],
   inputs: ['survey']
 })
 
