d02c4123a33a6a7264fefd6cd0caec43c754f778
Justin <justinowenjacot@icloud.com>
2016-03-28 15:19:53 -0400

    prepush

--- app/pages/begin-survey/begin-survey.page.ts
+++ app/pages/begin-survey/begin-survey.page.ts
@@ -1,3 +1,4 @@
+import {OnInit} from 'angular2/core';
 import {Page} from 'ionic-angular';
 import {NavController, NavParams, Button} from 'ionic-angular';
 import {Survey} from '../../models/survey/survey';
@@ -12,11 +13,22 @@ export class BeginSurveyPage {
   nav: NavController
   params: NavParams;
   survey: Survey;
+  surveyTime: String;
 
   constructor(nav: NavController, params: NavParams) {
     this.nav = nav;
     this.params = params;
     this.survey = this.params.get('survey');
+  }
+
+  ngOnInit() {
+    let
+      length = this.survey.questions.length,
+      surveyMinTime = Math.floor(length * 0.75),
+      surveyMaxTime = length * 1;
+
+    this.surveyTime = (`${surveyMinTime} - ${surveyMaxTime} mins`);
+    console.log(this.surveyTime);
   }
 
   viewSurvey(survey) {
