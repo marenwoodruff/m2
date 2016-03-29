00a36534c780095aa52b2787258ff039d7625a62
Nick Leach <nickleach22@gmail.com>
2016-03-29 11:39:24 -0400

    event search functionality

--- app/components/events/events.component.ts
+++ app/components/events/events.component.ts
@@ -1,16 +1,50 @@
-/**
- * Created by Abbey on 3/10/2016.
- */
-import {Component,OnInit,OnDestroy, Input} from 'angular2/core';
-import {List, Button} from 'ionic-angular';
+import {Component, OnChanges, Input} from 'angular2/core';
+import {List, Button, Searchbar, Icon} from 'ionic-angular';
 import {EventListItemComponent} from "./eventListItem.component";
 import {EventService} from "../../service/event.service";
+import {Event} from '../../models/events/event';
 
 @Component({
     selector: 'events',
     templateUrl: 'build/components/events/events.component.html',
-    directives: [List, EventListItemComponent, Button],
-    inputs:['events']
+    directives: [List, EventListItemComponent, Button, Searchbar, Icon],
+    inputs:['events'],
+    providers:[EventService]
 })
 
-export class EventsComponent {}
+export class EventsComponent implements OnChanges{
+  private _eventsApi:EventService;
+  searchQuery: string = '';
+  eventsSearch:Event[];
+  @Input() events;
+
+  constructor(eventService:EventService) {
+    this._eventsApi = eventService;
+
+  }
+  ngOnChanges(){
+    this.initializeItems();
+  }
+
+
+  initializeItems() {
+    this.eventsSearch = this.events;
+  }
+
+  getEvents(searchBar) {
+    this.initializeItems();
+
+    var q = searchBar.value;
+
+    if (q.trim() == '') {
+      return;
+    }
+
+    this.eventsSearch = this.eventsSearch.filter((v) => {
+      if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+        return true;
+      }
+      return false;
+    })
+  }
+}
