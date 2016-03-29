ff7ccb3a28958e628c466939fbcd8c7a047b99f3
Abbey Gwayambadde <ophedian@gmail.com>
2016-03-29 13:57:48 -0400

    Revert "updated answer component / added survey flow"

--- app/components/answer/answer.component.ts
+++ app/components/answer/answer.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {Item, Label} from 'ionic-angular';
+import {Item, Label, RadioGroup, RadioButton} from 'ionic-angular';
 import {RadioButtonComponent} from '../radio-button/radio-button.component';
 import {TextboxComponent} from '../textbox/textbox.component';
 import {CheckboxComponent} from '../checkbox/checkbox.component';
@@ -8,7 +8,7 @@ import {Answer} from '../../models/survey/answer';
 @Component({
   selector: 'answer',
   templateUrl: 'build/components/answer/answer.component.html',
-  directives: [Item, Label, RadioButtonComponent, TextboxComponent, CheckboxComponent],
+  directives: [Item, Label, RadioGroup, RadioButton, RadioButtonComponent, TextboxComponent, CheckboxComponent],
   inputs: ['answer']
 })
 
