a9d8883a00b0b7f1753bf1bfba21bf1f7cfb8c6f
Nick Leach <nickleach22@gmail.com>
2016-03-31 18:07:46 -0400

    show/hide arrows

--- app/pages/event/event.page.ts
+++ app/pages/event/event.page.ts
@@ -9,35 +9,34 @@ import {Event} from '../../models/events/event';
 
 @Page({
     templateUrl: 'build/pages/event/event.page.html',
-    directives: [EventComponent, SurveysComponent, SessionsComponent],
-    providers:[SurveyService]
+    directives: [EventComponent, SurveysComponent, SessionsComponent]
+    // providers:[SurveyService]
 })
 
 export class EventPage implements OnInit, OnDestroy{
   params: NavParams;
   event: Event;
-  page: string = "overview";
-  private _surveyApi: SurveyService;
+  // private _surveyApi: SurveyService;
   public surveys: Survey[];
 
   constructor(surveyService:SurveyService, navParams: NavParams) {
     this.params = navParams;
-    this._surveyApi = surveyService;
+    // this._surveyApi = surveyService;
   }
 
   ngOnInit():any{
     this.event = this.params.get('event');
-    this._surveyApi.surveys.subscribe(
-      surveys => this.surveys = surveys,
-      err => console.log('SurveysComponent subscribe error:', err),
-      () =>  console.log('finished subscribing to surveys')
-    );
+    // this._surveyApi.surveys.subscribe(
+    //   surveys => this.surveys = surveys,
+    //   err => console.log('SurveysComponent subscribe error:', err),
+    //   () =>  console.log('finished subscribing to surveys')
+    // );
 
     // this._surveyApi.getSurveys(null, this.event.id);
 
   }
   ngOnDestroy() {
-    this._surveyApi.surveys.unsubscribe();
+    // this._surveyApi.surveys.unsubscribe();
   }
 
 }
