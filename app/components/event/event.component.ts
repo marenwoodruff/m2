3fa80f02578590b722038ca3ddc248b6a9c08f45
Nick Leach <nickleach22@gmail.com>
2016-04-01 12:00:20 -0400

    styled event page

--- app/components/event/event.component.ts
+++ app/components/event/event.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit, OnDestroy, Input} from 'angular2/core';
-import {List, Item, NavController} from 'ionic-angular';
+import {Component} from 'angular2/core';
+import {List, Item, Button} from 'ionic-angular';
 import {Event} from '../../models/events/event';
 import {SessionComponent} from '../session/session.component';
 import {EventLocationComponent} from '../event-location/event-location.component';
+import {DateFormatPipe, FromUnixPipe} from 'angular2-moment';
 
 @Component({
   selector: 'event',
   templateUrl: 'build/components/event/event.component.html',
   inputs:['event'],
-  directives: [List, Item, SessionComponent, EventLocationComponent],
+  directives: [List, Item, SessionComponent, EventLocationComponent, Button],
+  pipes:[DateFormatPipe, FromUnixPipe]
 })
 
 export class EventComponent{}
