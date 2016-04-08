f93707fd15e45a680263e34ae744be48b30b91b1
Cori Fowler <fowler.cori@gmail.com>
2016-04-08 12:08:49 -0400

    surveys component handles replacing new survey with in progress if available

--- app/components/surveys/surveys.component.ts
+++ app/components/surveys/surveys.component.ts
@@ -1,12 +1,29 @@
-import {Component, forwardRef} from 'angular2/core';
+import {Component, forwardRef, OnChanges} from 'angular2/core';
 import {List, Item} from 'ionic-angular';
 import {SurveyDescriptionComponent} from '../survey-description/survey-description.component';
+import {Survey} from '../../models/survey/survey';
 
 @Component({
   selector: 'surveys',
   templateUrl: 'build/components/surveys/surveys.component.html',
   directives: [List, Item, forwardRef(() => SurveyDescriptionComponent)],
-  inputs:['surveys', 'surveysInProgress']
+  inputs:['surveys', 'surveysInProgress', 'startedSurveys']
 })
 
-export class SurveysComponent {}
+export class SurveysComponent implements OnChanges {
+  surveys: Survey[];
+  startedSurveys: Survey[];
+
+  ngOnChanges() {
+    if (this.surveys && this.startedSurveys) {
+      this.surveys.forEach((survey, index) => {
+        this.startedSurveys.forEach((startedSurvey, sIndex) => {
+          if (survey.id === startedSurvey.id) {
+            this.surveys[index] = startedSurvey;     
+          }
+        });
+      });
+    }  
+  }
+
+}
