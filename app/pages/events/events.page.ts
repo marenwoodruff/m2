1c8df376d27c7ff991f5281a02d3522d3f8e9493
Cori Fowler <fowler.cori@gmail.com>
2016-04-08 16:10:56 -0400

    reverted back

--- app/pages/events/events.page.ts
+++ app/pages/events/events.page.ts
@@ -31,7 +31,6 @@ export class EventsPage implements OnInit, OnDestroy {
             this.events = events
             this.getUpcomingEvents(events);
             this.getPastEvents(events);
-            console.log(this.events);
           },
           err => console.log("EventsComponent events subscribe error: ", err),
           () => console.log("Finished subscribing to events")
