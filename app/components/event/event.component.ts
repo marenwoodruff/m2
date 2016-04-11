525b3062984d67e0e3745ac25aa1d1b60d7f4ba8
Cori Fowler <fowler.cori@gmail.com>
2016-04-11 13:37:33 -0400

    added window open to view linkedin twitter of speaker for event. working on inappbrowser plugin

--- app/components/event/event.component.ts
+++ app/components/event/event.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {NavController, NavParams, List, Item, Button} from 'ionic-angular';
+import {NavController, NavParams, List, Item, Button, Platform} from 'ionic-angular';
 import {Event} from '../../models/events/event';
 import {SessionComponent} from '../session/session.component';
 import {EventLocationComponent} from '../event-location/event-location.component';
@@ -17,12 +17,18 @@ import {RegistrationPage} from '../../pages/registration/registration.page';
 export class EventComponent{
   event: Event
 
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController, private platform: Platform) {
   }
 
   private register(event): void {
     this.nav.push(RegistrationPage, {
       event: event
+    });
+  }
+
+  public launchUrl(url: string): void {
+    this.platform.ready().then(() => {
+      window.open(url, "_system", "location=true");
     });
   }
 }
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="main/ambient/cordova/index.d.ts" />
 /// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="main/definitions/moment/index.d.ts" />
+/// <reference path="main/ambient/cordova/plugins/inappbrowser/index.d.ts" />
 /// <reference path="./window.d.ts" />
