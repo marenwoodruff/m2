61e23a7ed1da284b53cf90bc07f3378962adcfe6
Justin <justinowenjacot@icloud.com>
2016-04-11 17:25:54 -0400

    fetching latest

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -8,53 +8,72 @@ import {Button, List, Item, TextInput, Label, Platform} from 'ionic-angular';
 })
 
 export class LoginComponent {
+  code: String;
+  state: String;
 
   constructor(private platform: Platform) {
   }
 
-  launch(url) {
-    this.platform.ready().then(() => {
-      // alert('just to see if the... um... just to see if its even...');
-      cordova.InAppBrowser.open(url, "_system", "location=true");
-    });
-  }
-
   login() {
-    this.launch("https://google.com")
-    // this.platform.ready().then(() => {
-
-
-    //   // this.linkedInLogin().then((success) => {
-    //   //   // alert(success.access_token);
-    //   // }, (error) => {
-    //   //   alert(error);
-    //   // });
-    // });
+    this.platform.ready().then(() => {
+      this.linkedInLogin().then((success) => {
+        this.code = success.code;
+        this.state = success.state;
+      }, (error) => {
+        alert(error);
+      });
+    });
 
   }
 
-  // linkedInLogin() {
-  //   return new Promise(function(resolve, reject) {
-  //     var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile");
-  //     browserRef.addEventListener('loadstop', (event) => {
-  //       if ((event.url).indexOf("http://localhost:8100") === 0) {
-  //         browserRef.removeEventListener("exit", (event) => { });
-  //         browserRef.close();
-  //         var responseParameters = ((event.url).split("#")[1]).split("&");
-  //         var parsedResponse = {};
-  //         for (var i = 0; i < responseParameters.length; i++) {
-  //           parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
-  //         }
-  //         if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
-  //           resolve(parsedResponse);
-  //         } else {
-  //           reject("Problem authenticating with LinkedIn");
-  //         }
-  //       }
-  //     });
-  //     browserRef.addEventListener("exit", function(event) {
-  //       reject("The LinkedIn sign in flow was canceled");
-  //     });
-  //   });
-  // }
+    linkedInLogin() {
+      return new Promise(function(resolve, reject) {
+        var browserRef = window.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_system", "location=true");
+
+        browserRef.addEventListener('loadstop', (event) => {
+          if ((event.url).indexOf("http://localhost:8100") === 0) {
+            browserRef.removeEventListener("exit", (event) => { });
+            browserRef.close();
+            var responseParameters = ((event.url).split("?")[1]).split("&");
+            var parsedResponse = {
+              code: undefined,
+              state: null,
+            };
+            for (var i = 0; i < responseParameters.length; i++) {
+              parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+            }
+            console.log(parsedResponse);
+            if (parsedResponse.code !== undefined && parsedResponse.code !== null) {
+              resolve(parsedResponse);
+            } else {
+              reject("Problem authenticating with LinkedIn");
+            }
+          }
+        });
+        browserRef.addEventListener("exit", function(event) {
+          reject("The LinkedIn sign in flow was canceled");
+        });
+        // var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_system", "location=true");
+
+        // browserRef.addEventListener('loadstop', (event) => {
+        //   if ((event.url).indexOf("http://localhost:8100") === 0) {
+        //     browserRef.removeEventListener("exit", (event) => { });
+        //     browserRef.close();
+        //     var responseParameters = ((event.url).split("#")[1]).split("&");
+        //     var parsedResponse = {};
+        //     for (var i = 0; i < responseParameters.length; i++) {
+        //       parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+        //     }
+        //     if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
+        //       resolve(parsedResponse);
+        //     } else {
+        //       reject("Problem authenticating with LinkedIn");
+        //     }
+        //   }
+        // });
+        // browserRef.addEventListener("exit", function(event) {
+        //   reject("The LinkedIn sign in flow was canceled");
+        // });
+      });
+    }
 }
