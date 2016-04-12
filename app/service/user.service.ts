4d37e261375cbebfcdadcf80a2297920be3eebc8
Nick Leach <nickleach22@gmail.com>
2016-04-12 14:48:06 -0400

    scaffolded user service

--- app/service/user.service.ts
+++ app/service/user.service.ts
@@ -1,9 +1,44 @@
-import {Injectable} from 'angular2/core';
+import {Injectable, EventEmitter} from 'angular2/core';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/Rx';
+
+import {User} from '../models/user/user';
+import {UserEvent} from '../models/user/userEvent';
+import {UserSurvey} from '../models/user/userSurvey';
+
 
 @Injectable()
 export class UserService {
-    constructor() {
+    private _api:Http;
+    userSurveys:EventEmitter<UserSurvey[]> = new EventEmitter();
+    userEvents:EventEmitter<UserEvent[]> = new EventEmitter();
+
+    constructor(private http:Http) {
+      this._api = http;
+    };
+
+    public getUserEvents():void {
+
+    }
+
+    public getUserSurveys():void {
+
+    }
+
+    public updateUser():void {
+
+    }
+
+    public updateUserEvent():void {
+
+    }
+
+    public deleteUserEven():void {
+
+    }
+
+    public deleteUser():void {
 
     }
 }
-
