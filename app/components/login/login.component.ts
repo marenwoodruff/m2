ac168cb4e2197e70ab4115620803799bbcaa69bb
Justin <justinowenjacot@icloud.com>
2016-04-12 15:26:25 -0400

    twitter work'

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,23 +1,28 @@
-import {Component, OnInit, OnDestroy, Input} from 'angular2/core';
+import {EventEmitter, Component, OnInit, OnDestroy, Input} from 'angular2/core';
 import { Inject } from 'angular2/core';
+import {TwitterService} from '../../service/twitter.service';
 import {Http} from 'angular2/http';
 import {Button, List, Item, TextInput, Label, Platform} from 'ionic-angular';
 
 @Component({
   selector: 'login',
   templateUrl: 'build/components/login/login.component.html',
-  directives: [Button, List, Item, TextInput, Label]
+  directives: [Button, List, Item, TextInput, Label],
+  providers: [TwitterService]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   linkedInCode: String;
   linkedInState: String;
   twitterCode: String;
   twitterState: String;
+  twitterCredentials: Object;
+  private twitterSubscription: EventEmitter<any>;
   http: Http;
 
-  constructor(private platform: Platform, @Inject(Http) http: Http) {
+  constructor(private platform: Platform, @Inject(Http) http: Http, private _twitterApi: TwitterService) {
     this.http = http;
+    this.twitterCredentials = undefined;
   }
 
   login(media) {
@@ -31,12 +36,7 @@ export class LoginComponent {
         });
       }
       if (media === 'Twitter') {
-        this.twitterLogin().then((success) => {
-          this.twitterCode = success.code;
-          this.twitterState = success.state;
-        }, (error) => {
-          alert(error);
-        });
+        this.twitterLogin();
       }
     });
 
@@ -69,45 +69,32 @@ export class LoginComponent {
         browserRef.addEventListener("exit", function(event) {
           reject("The LinkedIn sign in flow was canceled");
         });
-        setTimeout(function() {
-          console.log(browserRef.location.pathname, '==========');
-        }, 3000);
-
       });
     }
 
     twitterLogin() {
-      const encodedAccessKey = 'CWNkbHZKMkU0cEY2RzdSWE5KM0pnMGlqeTk6ek1oaUd0V2NyeXFhQVhTZFp3eUNhYWhtOW1nUnFxWGtTWTBCUDg1em5jc3pGdWxISTQNCg==';
-      return new Promise(function(resolve, reject) {
-        var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+      this._twitterApi.getBearer();
+      debugger
+    }
 
-        browserRef.addEventListener('loadstart', (event) => {
-          if ((event.url).indexOf("http://localhost:8100") === 0) {
-            browserRef.removeEventListener("exit", (event) => { });
-            browserRef.close();
-            var responseParameters = ((event.url).split("?")[1]).split("&");
-            var parsedResponse = {
-              code: undefined,
-              state: null,
-            };
-            for (var i = 0; i < responseParameters.length; i++) {
-              parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
-            }
-            console.log(parsedResponse);
-            if (parsedResponse.code !== undefined && parsedResponse.code !== null) {
-              resolve(parsedResponse);
-            } else {
-              reject("Problem authenticating with LinkedIn");
-            }
-          }
-        });
-        browserRef.addEventListener("exit", function(event) {
-          reject("The LinkedIn sign in flow was canceled");
-        });
-        setTimeout(function() {
-          console.log(browserRef.location.pathname, '==========');
-        }, 3000);
+    ngOnInit(): any {
+      this.twitterSubscription = this._twitterApi.twitterCredentials.subscribe(
+        (twitterCredentials) => {
+          this.twitterCredentials = twitterCredentials;
+          alert(twitterCredentials);
+            // return {
+            //   surveyId: sip.id,
+            //   lastQuestionId,
+            // }
+        },
+        err => console.log('twitterService subscribe error:', err),
+        () => {
+          console.log('finished subscribing to twitter service')
+        }
+      );
+    }
 
-      });
+    ngOnDestroy() {
+      this.twitterSubscription.unsubscribe();
     }
 }
