3395b38ce3533723d197c0b982c0f244bfa64179
Justin <justinowenjacot@icloud.com>
2016-04-12 12:38:40 -0400

    more authenitcation work

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit, OnDestroy, Input} from 'angular2/core';
+import { Inject } from 'angular2/core';
+import {Http} from 'angular2/http';
 import {Button, List, Item, TextInput, Label, Platform} from 'ionic-angular';
 
 @Component({
@@ -8,29 +10,43 @@ import {Button, List, Item, TextInput, Label, Platform} from 'ionic-angular';
 })
 
 export class LoginComponent {
-  code: String;
-  state: String;
+  linkedInCode: String;
+  linkedInState: String;
+  twitterCode: String;
+  twitterState: String;
+  http: Http;
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform, @Inject(Http) http: Http) {
+    this.http = http;
   }
 
-  login() {
+  login(media) {
     this.platform.ready().then(() => {
-      this.linkedInLogin().then((success) => {
-        this.code = success.code;
-        this.state = success.state;
-      }, (error) => {
-        alert(error);
-      });
+      if (media === 'LinkedIn') {
+        this.linkedInLogin().then((success) => {
+          this.linkedInCode = success.code;
+          this.linkedInState = success.state;
+        }, (error) => {
+          alert(error);
+        });
+      }
+      if (media === 'Twitter') {
+        this.twitterLogin().then((success) => {
+          this.twitterCode = success.code;
+          this.twitterState = success.state;
+        }, (error) => {
+          alert(error);
+        });
+      }
     });
 
   }
 
     linkedInLogin() {
       return new Promise(function(resolve, reject) {
-        var browserRef = window.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_system", "location=true");
+        var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
 
-        browserRef.addEventListener('loadstop', (event) => {
+        browserRef.addEventListener('loadstart', (event) => {
           if ((event.url).indexOf("http://localhost:8100") === 0) {
             browserRef.removeEventListener("exit", (event) => { });
             browserRef.close();
@@ -53,27 +69,45 @@ export class LoginComponent {
         browserRef.addEventListener("exit", function(event) {
           reject("The LinkedIn sign in flow was canceled");
         });
-        // var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_system", "location=true");
+        setTimeout(function() {
+          console.log(browserRef.location.pathname, '==========');
+        }, 3000);
 
-        // browserRef.addEventListener('loadstop', (event) => {
-        //   if ((event.url).indexOf("http://localhost:8100") === 0) {
-        //     browserRef.removeEventListener("exit", (event) => { });
-        //     browserRef.close();
-        //     var responseParameters = ((event.url).split("#")[1]).split("&");
-        //     var parsedResponse = {};
-        //     for (var i = 0; i < responseParameters.length; i++) {
-        //       parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
-        //     }
-        //     if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
-        //       resolve(parsedResponse);
-        //     } else {
-        //       reject("Problem authenticating with LinkedIn");
-        //     }
-        //   }
-        // });
-        // browserRef.addEventListener("exit", function(event) {
-        //   reject("The LinkedIn sign in flow was canceled");
-        // });
+      });
+    }
+
+    twitterLogin() {
+      const encodedAccessKey = 'CWNkbHZKMkU0cEY2RzdSWE5KM0pnMGlqeTk6ek1oaUd0V2NyeXFhQVhTZFp3eUNhYWhtOW1nUnFxWGtTWTBCUDg1em5jc3pGdWxISTQNCg==';
+      return new Promise(function(resolve, reject) {
+        var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+
+        browserRef.addEventListener('loadstart', (event) => {
+          if ((event.url).indexOf("http://localhost:8100") === 0) {
+            browserRef.removeEventListener("exit", (event) => { });
+            browserRef.close();
+            var responseParameters = ((event.url).split("?")[1]).split("&");
+            var parsedResponse = {
+              code: undefined,
+              state: null,
+            };
+            for (var i = 0; i < responseParameters.length; i++) {
+              parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+            }
+            console.log(parsedResponse);
+            if (parsedResponse.code !== undefined && parsedResponse.code !== null) {
+              resolve(parsedResponse);
+            } else {
+              reject("Problem authenticating with LinkedIn");
+            }
+          }
+        });
+        browserRef.addEventListener("exit", function(event) {
+          reject("The LinkedIn sign in flow was canceled");
+        });
+        setTimeout(function() {
+          console.log(browserRef.location.pathname, '==========');
+        }, 3000);
+
       });
     }
 }
