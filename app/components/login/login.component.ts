682d99d32550a0383c67ea5d4b3786cc5369fa8e
Justin <justinowenjacot@icloud.com>
2016-04-13 17:16:59 -0400

    prepush

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import {EventEmitter, Component, OnInit, OnDestroy, Input} from 'angular2/core';
 import { Inject } from 'angular2/core';
 import {TwitterService} from '../../service/twitter.service';
+import {LinkedInService} from '../../service/linkedin.service';
 import {Http} from 'angular2/http';
 import {Button, List, Item, TextInput, Label, Platform} from 'ionic-angular';
 
@@ -8,32 +9,29 @@ import {Button, List, Item, TextInput, Label, Platform} from 'ionic-angular';
   selector: 'login',
   templateUrl: 'build/components/login/login.component.html',
   directives: [Button, List, Item, TextInput, Label],
-  providers: [TwitterService]
+  providers: [TwitterService, LinkedInService]
 })
 
 export class LoginComponent implements OnInit, OnDestroy {
-  linkedInCode: String;
-  linkedInState: String;
-  twitterCode: String;
-  twitterState: String;
+  linkedInCredentials: any;
+  access_token: any;
   twitterCredentials: any;
   private twitterSubscription: EventEmitter<any>;
+  private linkedInSubscription: EventEmitter<any>;
+
   http: Http;
 
-  constructor(private platform: Platform, @Inject(Http) http: Http, private _twitterApi: TwitterService) {
+  constructor(private platform: Platform, @Inject(Http) http: Http, private _twitterApi: TwitterService, private _linkedInApi: LinkedInService) {
     this.http = http;
     this.twitterCredentials = { access_token: null };
   }
 
+
+
   login(media) {
     this.platform.ready().then(() => {
       if (media === 'LinkedIn') {
-        this.linkedInLogin().then((success) => {
-          this.linkedInCode = success.code;
-          this.linkedInState = success.state;
-        }, (error) => {
-          alert(error);
-        });
+        this.linkedInLogin()
       }
       if (media === 'Twitter') {
         this.twitterLogin();
@@ -43,51 +41,33 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
     linkedInLogin() {
-      return new Promise(function(resolve, reject) {
-        var browserRef = cordova.InAppBrowser.open("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=77afy8frauu9vo&redirect_uri=http://localhost:8100&state=51e48176-4b06-48d4-884b-9a3d643ee7d1&scope=r_basicprofile", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
-
-        browserRef.addEventListener('loadstart', (event) => {
-          if ((event.url).indexOf("http://localhost:8100") === 0) {
-            browserRef.removeEventListener("exit", (event) => { });
-            browserRef.close();
-            var responseParameters = ((event.url).split("?")[1]).split("&");
-            var parsedResponse = {
-              code: undefined,
-              state: null,
-            };
-            for (var i = 0; i < responseParameters.length; i++) {
-              parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
-            }
-            console.log(parsedResponse);
-            if (parsedResponse.code !== undefined && parsedResponse.code !== null) {
-              resolve(parsedResponse);
-            } else {
-              reject("Problem authenticating with LinkedIn");
-            }
-          }
-        });
-        browserRef.addEventListener("exit", function(event) {
-          reject("The LinkedIn sign in flow was canceled");
-        });
-      });
+      this._linkedInApi.auth();
     }
 
     twitterLogin() {
-      this._twitterApi.getBearer();
+      this._twitterApi.auth();
     }
 
     ngOnInit(): any {
       this.twitterSubscription = this._twitterApi.twitterCredentials.subscribe(
         (twitterCredentials) => {
+          console.log("seymour butts twitter", twitterCredentials);
           this.twitterCredentials = twitterCredentials;
-            // return {
-            //   surveyId: sip.id,
-            //   lastQuestionId,
-            // }
         },
         err => console.log('twitterService subscribe error:', err),
         () => {
           console.log('finished subscribing to twitter service')
+        }
+      );
+      this.linkedInSubscription = this._linkedInApi.linkedInCredentials.subscribe(
+        (linkedInCredentials) => {
+          console.log("seymour butts linked in", linkedInCredentials);
+          this.linkedInCredentials = linkedInCredentials;
+          this.access_token = linkedInCredentials.access_token;
+        },
+        err => console.log('LinkedIn Service subscribe error:', err),
+        () => {
+          console.log('finished subscribing to LinkedIn service')
         }
       );
     }
