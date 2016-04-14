431c464a5ce87fe3d0550c919a66ea8965b62486
Cori Fowler <fowler.cori@gmail.com>
2016-04-14 18:10:54 -0400

    linked in working sort of

--- app/components/login/login.component.ts
+++ app/components/login/login.component.ts
@@ -60,7 +60,7 @@ export class LoginComponent implements OnInit, OnDestroy {
       );
       this.linkedInSubscription = this._linkedInApi.linkedInCredentialsEmitter.subscribe(
         (linkedInCredentials) => {
-          alert("seymour butts linked in", linkedInCredentials);
+          this.access_token = linkedInCredentials.access_token;
           // this.linkedInCredentials = linkedInCredentials;
           // this.access_token = linkedInCredentials.access_token;
           // alert(this.access_token);
