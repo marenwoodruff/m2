0c2a392019477e108c58ccef2c25b980c30c45b5
Cori Fowler <fowler.cori@gmail.com>
2016-04-18 13:02:20 -0400

    added httpclient to handle get/post requests that require a proxy

--- app/service/event.service.ts
+++ app/service/event.service.ts
@@ -3,21 +3,20 @@ import {Http, HTTP_PROVIDERS, Headers} from 'angular2/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
 
+import {HttpClient} from './http-client.service';
+
 @Injectable()
 export class EventService {
 
-    private _api: Http;
     events: EventEmitter<Event[]> = new EventEmitter();
 
 
-    constructor(private http: Http) {
-        this._api = http;
-    }
+    constructor(private httpClient: HttpClient) { }
 
     public getEvents(): void {
       let headers = new Headers();
       headers.append('Access-Control-Allow-Origin', 'http://google.com');
-      this._api.get("http://localhost:1337/www.matrixres.com/apis/eventapi", { headers: headers })
+      this.httpClient.get("www.matrixres.com/apis/eventapi", headers)
             .map(res => <Event[]>res.json().events)
             .subscribe(
             events => this.events.emit(events),
