bbeef08446f8bf892f015f56fe6075efb2de1b9e
Cori Fowler <fowler.cori@gmail.com>
2016-04-19 13:54:35 -0400

    changed questionId to id in components, updated question image so it's not as stretched

--- app/models/survey/question.ts
+++ app/models/survey/question.ts
@@ -1,12 +1,12 @@
 import {Answer} from "./answer";
 
 export class Question {
-    questionId:number;
+    id:number;
     text:string;
     answer:Answer;
 
     constructor(questionId?:number, text?:string, answer?:Answer) {
-        this.questionId = questionId;
+        this.id = questionId;
         this.text = text;
         this.answer = answer;
     }
