8be3aafec8ca958793c30db1a93dcce3ddd02f5b
Nick Leach <nickleach22@gmail.com>
2016-04-21 15:54:08 -0400

    removed marketo module

--- app/components/session-registration/session-registration.component.ts
+++ app/components/session-registration/session-registration.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, AfterContentInit} from 'angular2/core';
 import {Button, List, Item, TextInput, Label} from 'ionic-angular';
-// import '../../modules/marketo';
+
 @Component({
   selector: 'session-registration',
   templateUrl: 'build/components/session-registration/session-registration.component.html',
