37666686ed67b2de87b3cce357b90fb9b51c3ef1
Nick Leach <nickleach22@gmail.com>
2016-04-25 13:49:29 -0400

    validations on login page

--- app/components/signup/signup.component.ts
+++ app/components/signup/signup.component.ts
@@ -4,16 +4,20 @@ import {UserService} from '../../service/user.service';
 import {User} from '../../models/user/user';
 import {EventsPage} from '../../pages/events/events.page';
 import {AuthorizationService} from '../../service/authorization.service';
+import {LoaderComponent} from '../loader/loader.component';
 
 
 @Component({
   selector: 'signup',
   templateUrl: 'build/components/signup/signup.component.html',
-  directives: [Button, List, Item, TextInput, Label]
+  directives: [Button, List, Item, TextInput, Label, LoaderComponent]
 })
 
 export class SignupComponent implements OnInit {
+  private signingUp: boolean;
+  private errorMessage: string;
   private userSubscription: EventEmitter<User>;
+  private errorSubscription: EventEmitter<any>;
   private user:User = new User();
 
   constructor(
@@ -29,9 +33,19 @@ export class SignupComponent implements OnInit {
           this._navController.setRoot(EventsPage);
         }
       )
+    this.errorSubscription = this._authService.error.subscribe(
+      (error) => {
+        console.log(error);
+        this.signingUp = false;
+        if (error.message){
+          this.errorMessage = error.message;
+        }
+      }
+    )
   }
 
   signUp(){
+    this.signingUp = true;
     this._authService.createUser(this.user);
   }
 
