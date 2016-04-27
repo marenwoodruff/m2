baef4c801d5cd22731dccf152cae0ca20368eb2c
Cori Fowler <fowler.cori@gmail.com>
2016-04-27 11:51:52 -0400

    added email cordova plugin to send email for support

--- app/pages/support/support.page.ts
+++ app/pages/support/support.page.ts
@@ -4,4 +4,17 @@ import {Page, Button} from 'ionic-angular';
   templateUrl: 'build/pages/support/support.page.html',
   directives: [Button]
 })
-export class SupportPage {}
+export class SupportPage {
+
+  contactSupport() {
+    cordova.plugins.email.isAvailable((available) => {
+      if (available) {
+        cordova.plugins.email.open({
+          to: 'events@matrixres.com',
+          subject: 'MyMATRIX Mobile Help'
+        });
+      } 
+    });
+  }
+
+}
