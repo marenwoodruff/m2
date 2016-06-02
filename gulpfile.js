3fe9dee0fc4e0ac3002e992ecb40ed06a36be594
Nick Leach <nickleach22@gmail.com>
2016-06-02 16:53:15 -0400

    switched to new ionic cli

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ var isRelease = argv.indexOf('--release') > -1;
 
 gulp.task('watch', ['clean'], function(done){
   runSequence(
-    ['sass', 'html', 'fonts', 'scripts'],
+    ['sass', 'html', 'fonts', 'assets', 'scripts'],
     function(){
       gulpWatch('app/**/*.scss', function(){ gulp.start('sass'); });
       gulpWatch('app/**/*.html', function(){ gulp.start('html'); });
