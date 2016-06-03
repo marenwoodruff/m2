c5a8b28b62a733483b7d1a8558c4862c022f136d
Nick Leach <nickleach22@gmail.com>
2016-06-03 14:02:48 -0400

    fixed typings?

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="main/ambient/cordova/index.d.ts" />
 /// <reference path="main/ambient/cordova/plugins/inappbrowser/index.d.ts" />
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
 /// <reference path="main/ambient/firebase-client/index.d.ts" />
 /// <reference path="main/ambient/firebase-simplelogin/index.d.ts" />
